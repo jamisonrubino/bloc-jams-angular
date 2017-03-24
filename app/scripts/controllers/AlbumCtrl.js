d42c61503b5dc0bae3e2d4725d76dac2a70d869c
jamisonrubino <jamison.rubino@gmail.com>
2017-03-24 15:24:07 -0700

    Checkpoint 5 assignment complete.

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,8 @@
 (function() {
 	function AlbumCtrl() {
-		this.album = this.albums["Picasso"];
-		for (var i=0; i < 12; i++) {
-			this.albums.push(angular.copy(albumPicasso));
-		}
+		this.albumData = albumPicasso;
 	}
 	angular
 		.module('blocJams')
-		.controller('CollectionCtrl', CollectionCtrl);
+		.controller('AlbumCtrl', AlbumCtrl);
  })();
---
+++ app/scripts/fixtures.js
@@ -0,0 +1,48 @@
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ],
+	 num: 1
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ],
+	 num: 2
+ };
+
+ var albumSpring = {
+     title: 'Spring',
+     artist: 'Time',
+     label: 'Four Seasons',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/14.png',
+     songs: [
+         { title: 'In', duration: '12:00' },
+         { title: 'Out', duration: '6:00' },
+         { title: 'Up', duration: '1:23'},
+         { title: 'Down', duration: '3:21' },
+         { title: 'Left/Right', duration: '9:15'}
+     ],
+	 num: 3
+ };
